5063e322ef1a87a2ab3e2e6710a95f1db19440c6
r2 <jbrayden35@gmail.com>
2023-05-07 12:29:04 -0700

    having trouble putting scenes in separate files

--- level_select.js
+++ level_select.js
@@ -5,9 +5,68 @@ class Level_select extends Phaser.Scene {
     preload(){
         this.load.path = "./assets/";
         this.load.image('space', 'space3.png');
+        this.load.image('blue', 'bluecheese.png');
+        this.load.image('cheese', 'cheese.png');
+        this.load.image('sat', 'shatturn.png');
+        this.load.image('earth', 'earth.png');
+        this.load.image('water', 'water9.png');
     }
 
     create() {
+        this.blue = this.add.image(
+            400,//x
+            500,//y
+            'blue',//imagename
+            )
+            this.blue.setScale(1, -1);
+            this.blue.setDepth(1)
+            this.blue.setScale(.35) //resize
+        
+        this.cheese = this.add.image(
+            200,//x
+            300,//y
+            'cheese',//imagename
+            )
+            this.cheese.setScale(1, -1);
+            this.cheese.setDepth(1)
+            this.cheese.setScale(.3) //resize
 
+        this.sat = this.add.image(
+            900,//x
+            750,//y
+            'sat',//imagename
+            )
+            this.sat.setScale(1, -1);
+            this.sat.setDepth(1)
+            this.sat.setScale(2) //resize
+
+        this.earth = this.add.image(
+            1500,//x
+            200,//y
+            'earth',//imagename
+            )
+            this.earth.setScale(1, -1);
+            this.earth.setDepth(1)
+            this.earth.setScale(.3) //resize
+
+        this.water = this.add.image(
+            1500,//x
+            900,//y
+            'water',//imagename
+            )
+            this.water.setScale(1, -1);
+            this.water.setDepth(1)
+            this.water.setScale(.3) //resize
+
+        const space = this.add.image(200, 0, 'space');
+        //space.scale(.5);
+        space.setOrigin(0);
+        space.setDepth(0);
+
+        this.blue.background = this.back;
+        this.sat.background = this.back;
+        this.earth.background = this.back;
+        this.water.background = this.back;
+        this.cheese.background = this.back;
     }
 }
---
+++ title.js
@@ -0,0 +1,71 @@
+
+class Title extends Phaser.Scene {
+    constructor() {
+        super('title');
+    }
+    preload(){
+        this.load.path = "./assets/";
+        this.load.image('space', 'space3.png');
+        this.load.spritesheet('met', 'metss.png', {
+            frameWidth: 120,
+            frameHeight: 120
+        });
+    }
+    create() {
+        this.imageObject = this.add.sprite(
+            1500,//x
+            700,//y
+            'met',//imagename
+        );
+        this.imageObject.setDepth(1);
+        this.imageObject.setScale(5); //resize
+        this.anims.create({
+            key: 'met',
+            frames: this.anims.generateFrameNumbers('met', {
+                start: 0,
+                end: 8
+            }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.imageObject.anims.play('met', true);
+
+        //this.image.add()
+        this.titleob = this.add.text(
+            600,//x
+            200,//y
+            'Sentient Meteor',//imagename
+            {
+                font: "100px Impact",
+                color: "#FF0",
+                align: "center"
+            }
+            )
+            //this.title.setDepth(1)
+            this.titleob.setDepth(1);
+            this.titleob.setScale(1) //resize
+            const instructions = this.add.text(200, 500, "HOW TO PLAY:\nSmash spaceships with your bod\nPress z to shoot fireballs at alien houses\nDon’t get hit by lasers!", { fontSize: '40px', fill: '#ff0' });
+            instructions.setDepth(1);
+            //this.add.text(50,50, "Adventure awaits!").setFontSize(50);
+        //this.add.text(50,100, "Click anywhere to begin.").setFontSize(20);
+        const playText = this.add.text(200, 800, 'PLAY', { fontSize: '100px', fill: '#fff' });
+        playText.setDepth(1);
+        playText.setInteractive();
+        playText.on('pointerover', () => {
+            playText.setStyle({ fill: '#ff0' });
+        });
+        playText.on('pointerout', () => {
+            playText.setStyle({ fill: '#fff' });
+        });
+        playText.on('pointerdown', () => {
+            this.scene.start('level_select');
+        });
+
+        const space = this.add.image(200, 0, 'space');
+        //space.scale(.5);
+        space.setOrigin(0);
+        space.setDepth(0);
+
+        this.imageObject.background = this.back;
+    }
+}
