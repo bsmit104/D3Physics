14c56f094bc0ec866daf1cd6fef6cdd10082f89a
r2 <jbrayden35@gmail.com>
2023-05-09 12:57:12 -0700

    onto something with shooting

--- water9.js
+++ water9.js
@@ -1,14 +1,63 @@
 // import Player from "./Player.js";
+class LaserGroup extends Phaser.Physics.Arcade.Group
+{
+	constructor(scene) {
+		// Call the super constructor, passing in a world and a scene
+		super(scene.physics.world, scene);
+ 
+		// Initialize the group
+		this.createMultiple({
+			classType: Laser, // This is the class we create just below
+			frameQuantity: 30, // Create 30 instances in the pool
+			active: false,
+			visible: false,
+			key: 'laser'
+		})
+	}
 
+  fireLaser(x, y) {
+		// Get the first available sprite in the group
+		const laser = this.getFirstDead(false);
+		if (laser) {
+			laser.fire(x, y);
+		}
+	}
+ 
+}
+ 
+class Laser extends Phaser.Physics.Arcade.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y, 'laser');
+	}
+
+  fire(x, y) {
+		this.body.reset(x, y);
+ 
+		this.setActive(true);
+		this.setVisible(true);
+ 
+		this.setVelocityY(900);
+	}
+  preUpdate(time, delta) {
+		super.preUpdate(time, delta);
+ 
+		if (this.y <= 0) {
+			this.setActive(false);
+			this.setVisible(false);
+		}
+	}
+}
 class Water9 extends Phaser.Scene {
   constructor() {
     super('water9');
+    this.inputKeys;
   }
   preload() {
     this.load.path = "./assets/";
     this.load.image('space', 'space3.png');
     this.load.image('house', 'bluetower.png');
     this.load.image('met', 'meteor.png');
+    this.load.image('laser', 'fireball.png');
     /////
     // this.load.image('met', 'meteor.png');
     // this.load.image('fireball', 'fireball.png');
@@ -18,7 +67,32 @@ class Water9 extends Phaser.Scene {
     // this.load.scenePlugin('WeaponPlugin', 'lib/WeaponPlugin.js', null, 'weapons');
   }
 
+shootLaser() {
+    this.laserGroup.fireLaser(this.met.x, this.met.y - 20);
+  }
+
+  addEvents() {
+      // this.input.on('pointermove', (pointer) => {
+      //   this.met.x = pointer.x;
+      // });
+
+      this.input.on('pointerdown', pointer => {
+        this.shootLaser();
+      });
+
+		this.inputKeys = [
+			this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
+			this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER),
+		];
+	}
+
   create() {
+    this.addEvents();
+    
+    this.laserGroup = new LaserGroup(this);
+    this.laserGroup.setDepth(1);
+    const text = this.add.text(1700, 100, 'lives: ' + lives, { fontFamily: 'Arial', fontSize: 40, color: '#ffffff' });
+    text.setDepth(1);
     // this.player = new Player(this, 400, 300);
     // this.cursors = this.input.keyboard.createCursorKeys();
     // define variables
@@ -34,31 +108,59 @@ class Water9 extends Phaser.Scene {
     this.met.setDrag(this.DRAG);
 
     if (!firstwatervisit) {
-    // Define the duration and number of flashes
-    const duration = 2000; // Duration in milliseconds
-    const numFlashes = 4; // Number of times the sprite will flash
-    // Define the tween animation
-    this.tweens.add({
-      targets: this.met,
-      alpha: 0, // Make the sprite transparent
-      ease: 'Linear',
-      duration: duration / (2 * numFlashes), // Divide the duration evenly across the number of flashes
-      repeat: numFlashes - 1, // Number of additional flashes (subtracting the initial state)
-      yoyo: true, // Make the tween reverse back to its initial state
-      onComplete: () => {
-        // Reset the sprite's alpha to 1 (fully opaque) after the tween is complete
-        this.met.alpha = 1;
-      }
-    });
-  }
-  else {
-    firstwatervisit = false;
-  }
+      // Define the duration and number of flashes
+      const duration = 2000; // Duration in milliseconds
+      const numFlashes = 4; // Number of times the sprite will flash
+      // Define the tween animation
+      this.tweens.add({
+        targets: this.met,
+        alpha: 0, // Make the sprite transparent
+        ease: 'Linear',
+        duration: duration / (2 * numFlashes), // Divide the duration evenly across the number of flashes
+        repeat: numFlashes - 1, // Number of additional flashes (subtracting the initial state)
+        yoyo: true, // Make the tween reverse back to its initial state
+        onComplete: () => {
+          // Reset the sprite's alpha to 1 (fully opaque) after the tween is complete
+          this.met.alpha = 1;
+        }
+      });
+    }
+    else {
+      firstwatervisit = false;
+    }
 
     // set up Phaser-provided cursor key input
     cursors = this.input.keyboard.createCursorKeys();
 
     ///////
+    // this.bullets = new Bullets(this);
+
+    // this.input.on('pointermove', (pointer) => {
+
+    //   this.met.x = pointer.x;
+
+    // });
+
+    // this.input.on('pointerdown', (pointer) => {
+
+    //   this.bullets.fireBullet(this.met.x, this.met.y);
+
+    // });
+    // Create a group for the fireballs
+    // const fireballsGroup = this.physics.add.group();
+
+    // // Function to handle shooting fireballs
+    // function shootFireball() {
+    //   const fireball = fireballsGroup.create(met.x, met.y, 'fireball');
+    //   fireball.setVelocity(0, -200);
+    // }
+
+    // // Listen for the spacebar key press event
+    // const spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    // this.input.keyboard.on('keydown_SPACE', function () {
+    //   shootFireball();
+    // });
+    /////////////
     this.rectangle = this.add.rectangle(300, 400, 500, 200, 0xFFFFF);
     this.rectangle.setDepth(1);
 
@@ -95,6 +197,7 @@ class Water9 extends Phaser.Scene {
     this.house3.setDepth(1)
     this.house3.setScale(1) //resize
 
+
     const space = this.add.image(200, 0, 'space');
     //space.scale(.5);
     space.setOrigin(0);
@@ -129,7 +232,7 @@ class Water9 extends Phaser.Scene {
     // this.physics.world.enable(this.rectangle);
     // this.physics.world.enable(this.rectangle2);
     // this.physics.world.enable(this.rectangle4);
-    
+
     this.physics.add.existing(this.rectangle);
     this.physics.add.existing(this.rectangle2);
     this.physics.add.existing(this.rectangle4);
@@ -140,7 +243,7 @@ class Water9 extends Phaser.Scene {
     function togameover() {
       // Trigger the scene change here
       // For example:
-      if (lives == 0) {
+      if (lives == 1) {
         this.scene.start('gameover');
         lives = 3;
       }
@@ -172,8 +275,15 @@ class Water9 extends Phaser.Scene {
     // });
     // wrap physics object(s) .wrap(gameObject, padding)
     //this.physics.world.wrap(this.met, this.met.width * SCALE / 2);
-  }
 
+    this.inputKeys.forEach(key => {
+			// If key was just pressed down, shoot the laser. We use JustDown to make sure this only fires once.
+			if (Phaser.Input.Keyboard.JustDown(key)) {
+				this.shootLaser();
+			}
+		});
+  }
+  
 
   //   // Horizontal movement
   //   if (this.cursors.left.isDown) {
